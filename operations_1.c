ef4c1435a5771a7058af64f3fc40e2a81b1ce331
Valentyn Sudak <vsudak@f0r1s6.codam.nl>
2025-12-13 15:12:34 +0100

    just a start

---
+++ operations_1.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   operations_1.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: vsudak <vsudak@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/12/13 12:41:08 by vsudak        #+#    #+#                 */
+/*   Updated: 2025/12/13 12:49:19 by vsudak        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	sa(int *stack_a, int *stack_b)
+{
+	ft_print_dcs("sa\n");
+}
+
+void	sb(int *stack_a, int *stack_b)
+{
+	ft_print_dcs("sb\n");
+}
+
+void	ss(int *stack_a, int *stack_b)
+{
+	ft_print_dcs("ss\n");
+}
+
+void	pa(int *stack_a, int *stack_b)
+{
+	ft_print_dcs("pa\n");
+}
+
+void	pb(int *stack_a, int *stack_b)
+{
+	ft_print_dcs("pb\n");
+}
---
+++ operations_2.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   operations_2.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: vsudak <vsudak@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/12/13 12:41:56 by vsudak        #+#    #+#                 */
+/*   Updated: 2025/12/13 12:49:09 by vsudak        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ra(int *stack_a, int *stack_b)
+{
+	ft_print_dcs("ra\n");
+}
+
+void	rb(int *stack_a, int *stack_b)
+{
+	ft_print_dcs("rb\n");
+}
+
+void	rr(int *stack_a, int *stack_b)
+{
+	ft_print_dcs("rr\n");
+}
+
+void	rra(int *stack_a, int *stack_b)
+{
+	ft_print_dcs("rra\n");
+}
+
+void	rrb(int *stack_a, int *stack_b)
+{
+	ft_print_dcs("rrb\n");
+}
