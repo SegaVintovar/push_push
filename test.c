91a8d2163c88f27e00ad520b6d01fba5a5004007
vsudak <vsudak@student.codam.nl>
2025-12-22 11:52:07 +0100

    i think input validation is done in test.c

--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include "push_push_header.h"
 
 int is_number(char *arg)
 {
@@ -20,15 +21,54 @@ int is_number(char *arg)
 	return (1);
 }
 
-int is_unique(char **arg)
+int is_unique(stack a)
 {
-	
+	size_t	i;
+	size_t	p;
+
+	i = 0;
+	while (i < a.size)
+	{
+		p = i + 1;
+		while (a.arr[i] != a.arr[p] && p < a.size)
+			p++;
+		if (a.arr[i] == a.arr[p])
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int main()
+int main(int argc, char **argv)
 {
 	char str[6] = " -23a";
+	stack	a;
+	size_t	i;
 
-	printf("%d\n", is_number(str));
+	i = 1;
+	while (i < argc)
+	{
+		if (is_number(argv[i]) == 0)
+			return (0);
+		i++;
+	}
+	a.size = argc - 1;
+	a.arr = (int *)malloc(a.size * sizeof(int));
+	i = 0;
+	while (i < a.size)
+	{
+		a.arr[i] = ft_atoi(argv[i + 1]);
+		i++;
+	}
+	if (is_unique(a) == 0)
+		return (0);
+	else
+		i = 0;
+		while (i < a.size)
+		{
+			printf("%d\n", a.arr[i]);
+			i++;
+		}
+	//printf("%d\n", is_number(str));
 
 }
